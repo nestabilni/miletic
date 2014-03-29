f9012babea9d8674af89ddaf6ad742927e45ed9e
tomislav <rubyrails333@gmail.com>
2014-03-29 10:06:37 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Miletic::Application.config.secret_key_base = 'ccb45fd4be3a41ee08b60594e16d1bdf85c6cdf5e0e3063a601b366817525a21cc06ac1fb2b9cb5f505e9199781975eb755cf0a1f1e86eb38cd17cbe7c32373d'
